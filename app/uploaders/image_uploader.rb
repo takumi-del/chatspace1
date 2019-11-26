b3093ab49bfaaa919c098d9d6a9a7f6b218e50a8
suechan-suechan <suechan_0930@softbank.ne.jp>
2019-11-26 17:13:36 +0900

    setting S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,3 +1,16 @@
-include CarrierWave::MiniMagick
+class ImageUploader < CarrierWave::Uploader::Base
 
-  # process resize_to_fit: [800, 800]
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end
+#後略
