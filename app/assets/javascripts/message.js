ad52e84030873c0b20a2d91324d3ddfc6d143e3d
suechan-suechan <suechan_0930@softbank.ne.jp>
2019-11-28 12:28:34 +0900

    commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -46,10 +46,10 @@ $(function () {
       return false;
     })
   
-  var reloadMessages = function () {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var last_message_id = $('.message:last').data("message-id");
-      
+    var reloadMessages = function () {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        var last_message_id = $('.message:last').data("message-id");
+        
 
       $.ajax({ 
         url: "api/messages", 
