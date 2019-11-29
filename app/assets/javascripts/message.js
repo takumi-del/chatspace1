c50b02041727b4707b20ca3671b2062f28bb0aa4
suechan-suechan <suechan_0930@softbank.ne.jp>
2019-11-29 10:31:38 +0900

    change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function () {
 
   function buildHTML(message) {
-
+    console.log(message)
     image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : ""; 
 
     var html = `<div class="message" data-message-id="${message.id}"> 
@@ -20,7 +20,7 @@ $(function () {
             ${image}
           </div>
         </div>`
-    return html;
+    $('.messages').append(html); 
   }
   $('.new_message').on('submit', function(e){
     e.preventDefault();
@@ -51,24 +51,24 @@ $(function () {
         var last_message_id = $('.message:last').data("message-id");
         
 
-      $.ajax({ 
-        url: "api/messages", 
-        type: 'get', 
-        dataType: 'json',
-        data: {last_id: last_message_id} 
-      })
-      .done(function (messages) { 
-        var insertHTML = '';
-        messages.forEach(function (message) {
-          insertHTML = buildHTML(message); 
-          $('.messages').append(insertHTML);
+        $.ajax({ 
+          url: "api/messages", 
+          type: 'get', 
+          dataType: 'json',
+          data: {last_id: last_message_id} 
         })
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      })
-      .fail(function () {
-        alert('自動更新に失敗しました');
-      });
-    }
-  };
-  setInterval(reloadMessages, 5000);
-});
+        .done(function (messages) { 
+          var insertHTML = '';
+          messages.forEach(function (message) {
+            insertHTML = buildHTML(message); 
+            $('.messages').append(insertHTML);
+          })
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+        .fail(function () {
+          alert('自動更新に失敗しました');
+        });
+      }
+    };
+    // setInterval(reloadMessages, 7000);
+    });
